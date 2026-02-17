098dce3c3bf0e15b1dcf8f6cebcec1ed21bcfab5
Miguel Narvaez <83840366+MiguelN27@users.noreply.github.com>
2026-02-17 06:01:49 +0000

    last update

--- src/app.js
+++ src/app.js
@@ -10,11 +10,14 @@ window.onload = function() {
   const pronoun = ["the", "our"];
   const adj = ["great", "big"];
   const noun = ["jogger", "racoon"];
+  const ext = [".com", ".net", ".us", ".io"];
 
   for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++){
       for (let adjIndex = 0; adjIndex < adj.length; adjIndex++){
         for (let nounIndex = 0; nounIndex < noun.length; nounIndex++){
-          console.log(pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + ".com")
+          for (let extIndex = 0; extIndex < ext.length; extIndex++){
+            console.log(pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + ext[extIndex])
+          }
         }
       }
   }
